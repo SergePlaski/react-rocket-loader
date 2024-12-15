c28b5e6c18fcd35f67c08fa0dd51174c332f82e9
SergePlaski <splaski@gmail.com>
2024-12-14 23:25:20 -0500

    Changed from grid to flex content layout for better control over content sizing; Added content opacity transition on initial content load.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 import { useEffect, useState } from "react";
 import { LoadingRocket } from "./LoadingRocket";
 
-const LOADER_DELAY = 3000;
+const LOADER_DELAY = 2000;
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     const timeout = setTimeout(() => setIsLoading(false), LOADER_DELAY);
-
     // clean-up: reset timeout
     return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      // Add a delay to ensure content is rendered before transition
+      // requestAnimationFrame is a browser API function 
+      // and is more efficient than setTimeout for animations.
+      requestAnimationFrame(() => setIsMounted(true));
+    }
+  }, [isLoading]);
 
   return (
     <div className="app-container">
       {isLoading ? (
         <LoadingRocket />
       ) : (
-        <div className="content-container">
+        <div className={`content ${isMounted ? 'mounted' : ''}`}>
           {Array.from({ length: 9 }, (_, index) => (
-            <div className="content-block" key={index}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Laboriosam praesentium non fugiat odio aliquid, nisi quia veniam?
-              Maiores exercitationem nesciunt veniam reiciendis nisi a suscipit
-              aliquam illo, optio similique dicta ut? Provident ad,
-              reprehenderit quibusdam unde repudiandae enim.
-            </div>
+            <div className="content-block" key={index} />
           ))}
         </div>
       )}
